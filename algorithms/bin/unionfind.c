0530748908b99be109232a87cd9c4377998333c9
jonnii <me@jonnii.com>
2012-10-17 23:20:10 -0400

    move a bunch of stuff around, add path compression.

--- algorithms/bin/unionfind.c
+++ algorithms/bin/unionfind.c
@@ -3,237 +3,252 @@
 #include <string.h>
 
 struct union_state {
-	int num_components;
-	int num_ids;
-	int *ids;
-	int *sizes;
+  int num_components;
+  int num_ids;
+  int *ids;
+  int *sizes;
+
+  int path_compression;
+  int (*_connected)(struct union_state*, int, int);
+  void (*_connect)(struct union_state*, int, int);
 };
 
 typedef struct union_state * union_state_p;
 
-struct union_algorithm
-{
-	int (*_connected)(union_state_p, int, int);
-	void (*_connect)(union_state_p, int, int);
-};
-
-typedef struct union_algorithm * union_algorithm_p;
+typedef void (*apply_algorithm_p)(union_state_p);
 
 // quick find
 
 int qf_find(union_state_p state, int p)
 {
-	return state->ids[p];
+  return state->ids[p];
 }
 
 int qf_connected(union_state_p state, int p, int q)
 {
-	return qf_find(state, p) == qf_find(state, q);
+  return qf_find(state, p) == qf_find(state, q);
 }
 
 void qf_connect(union_state_p state, int p, int q)
 {
-	// ids of p and q
-	int pid = qf_find(state, p);
-	int qid = qf_find(state, q);
+  // ids of p and q
+  int pid = qf_find(state, p);
+  int qid = qf_find(state, q);
 
-	// if they're already part of the same component do nothing
-	if(pid == qid)
-	{
-		return;
-	}
+  // if they're already part of the same component do nothing
+  if(pid == qid)
+  {
+    return;
+  }
 
-	// set all pid and qid's equal
-	for(int i = 0 ; i < state->num_ids; i++)
-	{
-		if(state->ids[i] == pid)
-		{
-			state->ids[i] = qid;
-		}
-	}
+  // set all pid and qid's equal
+  for(int i = 0 ; i < state->num_ids; i++)
+  {
+    if(state->ids[i] == pid)
+    {
+      state->ids[i] = qid;
+    }
+  }
 
-	// we got a component
-	state->num_components--;
+  // we got a component
+  state->num_components--;
 }
 
 // quick union
 int qu_find(union_state_p state, int p)
 {
-	while(p != state->ids[p])
-	{
-		p = state->ids[p];
-	}
+  int root = p;
+  while(root != state->ids[root])
+  {
+    root = state->ids[root];
+  }
 
-	return p;
+  if(state->path_compression)
+  {
+    while (p != root)
+    {
+        int newp = state->ids[p];
+          state->ids[p] = root;
+          p = newp;
+      }
+  }
+
+  return root;
 }
 
 void qu_connect(union_state_p state, int p, int q)
 {
-	int i = qu_find(state, p);
-	int j = qu_find(state, q);
+  int i = qu_find(state, p);
+  int j = qu_find(state, q);
 
-	if(i == j)
-	{
-		return;
-	}
+  if(i == j)
+  {
+    return;
+  }
 
-	state->ids[i] = j;
-	state->num_components--;
+  state->ids[i] = j;
+  state->num_components--;
 }
 
 int qu_connected(union_state_p state, int p, int q)
 {
-	return qu_find(state, p) == qu_find(state, q);
+  return qu_find(state, p) == qu_find(state, q);
 }
 
 // weighted quick union
 
 void wqu_connect(union_state_p state, int p, int q)
 {
-	int i = qu_find(state, p);
-	int j = qu_find(state, q);
+  int i = qu_find(state, p);
+  int j = qu_find(state, q);
 
-	if(i == j)
-	{
-		return;
-	}
+  if(i == j)
+  {
+    return;
+  }
 
-	if(state->sizes[i] < state->sizes[j])
-	{
-		state->ids[i] = j;
-		state->sizes[j] += state->sizes[i];
-	}
-	else
-	{
-		state->ids[j] = i;
-		state->sizes[i] += state->sizes[j];
-	}
+  if(state->sizes[i] < state->sizes[j])
+  {
+    state->ids[i] = j;
+    state->sizes[j] += state->sizes[i];
+  }
+  else
+  {
+    state->ids[j] = i;
+    state->sizes[i] += state->sizes[j];
+  }
 
-	state->num_components--;
+  state->num_components--;
 }
 
-union_state_p run_union(union_algorithm_p algorithm, char *filename)
-{	
-	printf("processing file: %s\n", filename);
+union_state_p run_union(apply_algorithm_p algorithm, char *filename)
+{ 
+  printf("processing file: %s\n", filename);
 
-	FILE *handle = fopen(filename, "rt");
+  FILE *handle = fopen(filename, "rt");
 
-	if (handle == NULL)
-	{
-		perror ("Error opening file");
-		return NULL;
-	}
+  if (handle == NULL)
+  {
+    perror ("Error opening file");
+    return NULL;
+  }
 
-	union_state_p state = (union_state_p)calloc(1, sizeof(struct union_state));
-	fscanf(handle, "%d", &state->num_ids);
-	printf("reading num sites: %d\n", state->num_ids);
+  union_state_p state = (union_state_p)calloc(1, sizeof(struct union_state));
+  fscanf(handle, "%d", &state->num_ids);
+  printf("reading num sites: %d\n", state->num_ids);
 
-	state->ids = calloc(state->num_ids, sizeof(int));
-	state->sizes = calloc(state->num_ids, sizeof(int));
-	state->num_components = state->num_ids;
-	
-	for(int i = 0 ; i < state->num_ids ; i++)
-	{
-		state->ids[i] = i;
-		state->sizes[i] = 1;
-	}
+  state->ids = calloc(state->num_ids, sizeof(int));
+  state->sizes = calloc(state->num_ids, sizeof(int));
+  state->num_components = state->num_ids;
+  
+  algorithm(state);
 
-	int p = 0;
-	int q = 0;
-	while(fscanf(handle, "%d %d", &p, &q) > 0)
-	{
-		if(algorithm->_connected(state, p, q))
-		{
-			continue;
-		}
+  for(int i = 0 ; i < state->num_ids ; i++)
+  {
+    state->ids[i] = i;
+    state->sizes[i] = 1;
+  }
 
-		algorithm->_connect(state, p, q);
-	}
+  int p = 0;
+  int q = 0;
+  while(fscanf(handle, "%d %d", &p, &q) > 0)
+  {
+    if(state->_connected(state, p, q))
+    {
+      continue;
+    }
 
-	fclose(handle);
-	return state;
+    state->_connect(state, p, q);
+  }
+
+  fclose(handle);
+  return state;
 }
 
 void free_union_state(union_state_p state)
 {
-	free(state->ids);
-	free(state);
+  free(state->ids);
+  free(state);
 }
 
-union_algorithm_p create_quick_find()
+void create_quick_find(union_state_p state)
 {
-	union_algorithm_p algo = (union_algorithm_p)malloc(sizeof(struct union_algorithm));
-	algo->_connect = qf_connect;
-	algo->_connected = qf_connected;
-	return algo;
+  state->_connect = qf_connect;
+  state->_connected = qf_connected;
 }
 
-union_algorithm_p create_quick_union()
+void create_quick_union(union_state_p state)
 {
-	union_algorithm_p algo = (union_algorithm_p)malloc(sizeof(struct union_algorithm));
-	algo->_connect = qu_connect;
-	algo->_connected = qu_connected;
-	return algo;
+  state->_connect = qu_connect;
+  state->_connected = qu_connected;
 }
 
-union_algorithm_p create_weighted_quick_union()
+void create_weighted_quick_union(union_state_p state)
 {
-	union_algorithm_p algo = (union_algorithm_p)malloc(sizeof(struct union_algorithm));
-	algo->_connect = wqu_connect;
-	algo->_connected = qu_connected;
-	return algo;
+  state->_connect = wqu_connect;
+  state->_connected = qu_connected;
+  state->path_compression = 0;
 }
 
-union_algorithm_p create_algorithm(char *name)
+void create_weighted_quick_union_with_path_compression(union_state_p state)
 {
-	if(strncmp(name, "qf", 5) == 0)
-	{
-		return create_quick_find();
-	}
-	else if(strncmp(name, "qu", 5) == 0)
-	{
-		return create_quick_union();
-	}
-	else if(strncmp(name, "wqu", 5) == 0)
-	{
-		return create_weighted_quick_union();
-	}
-	else
-	{
-		return NULL;
-	}
+  state->_connect = wqu_connect;
+  state->_connected = qu_connected;
+  state->path_compression = 1;
+}
+
+apply_algorithm_p create_algorithm(char *name)
+{
+  if(strncmp(name, "qf", 5) == 0)
+  {
+    return create_quick_find;
+  }
+  else if(strncmp(name, "qu", 5) == 0)
+  {
+    return create_quick_union;
+  }
+  else if(strncmp(name, "wqu", 5) == 0)
+  {
+    return create_weighted_quick_union;
+  }
+  else if(strncmp(name, "wqupc", 5) == 0)
+  {
+    return create_weighted_quick_union_with_path_compression;
+  }
+  else
+  {
+    return NULL;
+  }
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc < 3)
-	{
-		printf("Usage: ./unionfind quickfind <filename>\n");
-		return 1;
-	}
+  if(argc < 3)
+  {
+    printf("Usage: ./unionfind quickfind <filename>\n");
+    return 1;
+  }
 
-	char *algorithm_name = argv[1];
-	char *filename = argv[2];
+  char *algorithm_name = argv[1];
+  char *filename = argv[2];
 
-	union_algorithm_p algorithm = create_algorithm(algorithm_name);
-	if(algorithm == NULL)
-	{
-		printf("unknown algorithm: %s\n", algorithm_name);
-		return 1;
-	}
+  apply_algorithm_p algorithm = create_algorithm(algorithm_name);
+  if(algorithm == NULL)
+  {
+    printf("unknown algorithm: %s\n", algorithm_name);
+    return 1;
+  }
 
-	union_state_p state = run_union(algorithm, filename);
+  union_state_p state = run_union(algorithm, filename);
 
-	if(state == NULL)
-	{
-		printf("couldn't create state\n");
-		return 1;
-	}
+  if(state == NULL)
+  {
+    printf("couldn't create state\n");
+    return 1;
+  }
 
-	printf("Found %d components\n", state->num_components);
-	
-	free_union_state(state);
-	free(algorithm);
-
-	return 0;
+  printf("Found %d components\n", state->num_components);
+  free_union_state(state);
+  
+  return 0;
 }
